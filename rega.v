809c5051d5c0a7c29a25d290bd6fe6163af76d7f
Akari Kato <a.kato03040@keio.jp>
2025-03-26 16:26:34 +0900

    logic remove_imem5.6

--- rega.v
+++ rega.v
@@ -1,6 +1,6 @@
 `include "pu.vh"
-module rega(input logic [`RASB:0] arad, brad, output logic [`WIDTH:0] a, b,
-	input we, input logic [`RASB:0] wad, input [`WIDTH:0] wd,
+module rega(input [`RASB:0] arad, brad, output logic [`WIDTH:0] a, b,
+	input we, input [`RASB:0] wad, input [`WIDTH:0] wd,
 	input clk, rst);
 	logic [`WIDTH:0] regar [`RAS:0];
 	always @(posedge clk or posedge rst) begin
